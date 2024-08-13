1694b92337316226658a38f127c29373dc5651bb
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-13 15:29:06 -0700

    Adjust Mastermind for creating an instance
    Provide instance methods for an initialization and rename variables

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -4,16 +4,33 @@ require_relative('code')
 
 # Mastermind stores a secret code and able to provide feedbacks from a guess
 class Mastermind
-  attr_reader :secret_code
+  attr_reader :key_pegs
+
+  def initialize
+    # Assume computer is the mastermind
+    computer_mastermind
+  end
+
+  def make_guess(guesses)
+    @key_pegs = guesses.map.with_index do |guess, i|
+      feedback(guess, i)
+    end
+    @key_pegs.shuffle
+  end
+
+  def win?
+    @key_pegs.uniq.length.eql?(1) && @key_pegs[0].eql('BLACK')
+  end
 
   private
 
   def computer_mastermind
-    @secret_code = Code.generate_code
+    @code_pegs = Code.generate_code
+    nil
   end
 
   def feedback(code, index)
-    peg = @secret_code.fetch(code, nil)
+    peg = @code_pegs.fetch(code, nil)
     if peg
       return peg.include?(index) ? 'WHITE' : 'BLACK'
     end
