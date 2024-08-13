a646a68a25a1501932eede92ec41b47531b1ea1f
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-12 17:47:02 -0700

    Refactor mastermind.rb to two classes
    Some parts in Mastermind class does not fit or make much sense.

---
+++ lib/code.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Code provides class methods to handle a mastermind's secret code based of a set of colors
+class Code
+  COLORS = %w[YELLOW ORANGE RED PINK VIOLET BLUE].freeze
+  COLORS_SET = COLORS.to_set
+
+  def self.generate_code
+    # Key = color code, Value = index of the color code as a set
+    code = Hash.new { |hash, key| hash[key] = Set[] }
+    4.times { |i| code[COLORS[Random.rand(6)]] << i }
+    code
+  end
+
+  def self.create_guess
+    4.times.map do |i|
+      color = nil
+      puts 'POSSIBLE COLORS: YELLOW, ORANGE, RED, PINK, VIOLET, BLUE'
+      loop do
+        puts "\nGuess no. #{i + 1}: "
+        color = gets.chomp.upcase
+        COLORS_SET.include?(color) ? break : puts('Invalid guess')
+      end
+      color
+    end
+  end
+end
--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,35 +1,17 @@
 # frozen_string_literal: true
 
-# Create and manage a mastermind game
+require_relative('code')
+
+# Mastermind stores a secret code and able to provide feedbacks from a guess
 class Mastermind
-  COLORS = %w[YELLOW ORANGE RED PINK VIOLET BLUE].freeze
-  COLORS_SET = COLORS.to_set
   MAX_GUESSES = 12
 
   attr_reader :secret_code
 
+  private
+
   def computer_mastermind
-    @secret_code = generate_code
-  end
-
-  def generate_code
-    # Key = color code, Value = index of the color code as a set
-    code = Hash.new { |hash, key| hash[key] = Set[] }
-    4.times { |i| code[COLORS[Random.rand(6)]] << i }
-    code
-  end
-
-  def create_guess
-    4.times.map do |i|
-      color = nil
-      puts 'POSSIBLE COLORS: YELLOW, ORANGE, RED, PINK, VIOLET, BLUE'
-      loop do
-        puts "\nGuess no. #{i + 1}: "
-        color = gets.chomp.upcase
-        COLORS_SET.include?(color) ? break : puts('Invalid guess')
-      end
-      color
-    end
+    @secret_code = Code.generate_code
   end
 
   def feedback(code, index)
