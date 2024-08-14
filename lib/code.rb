9ba8f8ea68d4183840bf2f1280921796fc9ffbd2
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-14 14:33:31 -0700

    Reformat to modify mastermind's feedback algo
    The feedback algo contains error when handling with
    duplicates of colors. This changes provide the necessary information
    for a different feedback algorithm that would appropriately deal with
    duplicates of colors guesses and code_pegs.

--- lib/code.rb
+++ lib/code.rb
@@ -6,10 +6,7 @@ class Code
   COLORS_SET = COLORS.to_set
 
   def self.generate
-    # Key = color code, Value = index of the color code as a set
-    code = Hash.new { |hash, key| hash[key] = Set[] }
-    4.times { |i| code[COLORS[Random.rand(6)]] << i }
-    code
+    format_code(4.times.map { COLORS[Random.rand(6)] })
   end
 
   def self.create_guess
@@ -18,10 +15,21 @@ class Code
     loop do
       puts "\nInput the valid colors separated by spaces, ex: 'yellow orange red pink'"
       colors = gets.chomp.upcase.split(' ')
-      break if colors.length == 4 && color.all? { |color| Code::COLORS_SET.include?(color) }
+      break if colors.length == 4 && colors.all? { |color| Code::COLORS_SET.include?(color) }
 
       puts('Invalid guess')
     end
-    colors
+    format_code(colors)
   end
+
+  ##
+  # Given an array of colors, format them to Hash: Key = color code, Value = indices of the color code as a set.
+  # Does not validate the input.
+  def self.format_code(colors)
+    code = Hash.new { |hash, key| hash[key] = Set[] }
+    colors.each_with_index { |color, i| code[color] << i }
+    code
+  end
+
+  private_class_method :format_code
 end
